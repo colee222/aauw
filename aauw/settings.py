75bb3f540194eb17bfedf02e7b5d9ad94e3b079f
Fay <nfsulli@gmail.com>
2020-03-29 22:36:36 -0400

    changed static in setting.py

--- aauw/settings.py
+++ aauw/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # for static files 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     
-STATICFILES_DIRS = (
+STATIC_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
